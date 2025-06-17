1c0a6ffa4685142345f8ba395f6c3603fda61f87
N-sam-sn <snsamsn4@gmail.com>
2025-06-17 15:09:39 +0300

    Update VP_.py

--- VP_.py
+++ VP_.py
@@ -28,7 +28,7 @@ def load_data():
         if col in df.columns:
             df[col] = df[col].apply(clean_number)
             df[col] = pd.to_numeric(df[col], errors="coerce")
-    for col in ["Менеджер", "Покупатель"]:
+    for col in ["Менеджер", "Покупатель","Регион","Добавить в план"]:
         if col in df.columns:
             df[col] = df[col].apply(clean)
 
