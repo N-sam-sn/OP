4d60c2d8dfc4f3872ac16b9a4e0d13d3a19cf923
N-sam-sn <snsamsn4@gmail.com>
2025-06-17 15:34:49 +0300

    Update VP_.py

--- VP_.py
+++ VP_.py
@@ -90,15 +90,15 @@ def multiselect_with_all(label, options):
 filtered_df = df.copy()
 
 
-if "Канал" in filtered_df.columns:
-    channels = sorted(filtered_df["Канал"].dropna().unique())
-    channel_selection = multiselect_with_all("Канал", channels)
-    filtered_df = filtered_df[filtered_df["Канал"].isin(channel_selection)]
-
 if "Отдел" in filtered_df.columns:
     departments = sorted(filtered_df["Отдел"].dropna().unique())
     department_selection = multiselect_with_all("Отдел", departments)
     filtered_df = filtered_df[filtered_df["Отдел"].isin(department_selection)]
+
+if "Канал" in filtered_df.columns:
+    channels = sorted(filtered_df["Канал"].dropna().unique())
+    channel_selection = multiselect_with_all("Канал", channels)
+    filtered_df = filtered_df[filtered_df["Канал"].isin(channel_selection)]
     
 if "Регион" in filtered_df.columns:
     regions = sorted(filtered_df["Регион"].dropna().unique())
