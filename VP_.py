c2e0827ddc6bd2c45f04994017eb67461a873163
N-sam-sn <snsamsn4@gmail.com>
2025-07-30 09:00:14 +0300

    Update VP_.py

--- VP_.py
+++ VP_.py
@@ -193,7 +193,7 @@ if not filtered_df.empty:
         </div>
     """
 
-    st.subheader("📋 Результаты на 28.07.2025")
+    st.subheader("📋 Результаты на 29.07.2025")
     st.markdown(summary_html, unsafe_allow_html=True)
 
     styled_html = df_result.style \
