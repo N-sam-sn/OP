a298745441f5a18ff1a595c27db713b575609ec2
N-sam-sn <snsamsn4@gmail.com>
2026-04-14 09:08:41 +0300

    Update VP_.py

--- VP_.py
+++ VP_.py
@@ -193,7 +193,7 @@ if not filtered_df.empty:
         </div>
     """
 
-    st.subheader("Результаты на 12.04.2026")
+    st.subheader("Результаты на 13.04.2026")
     st.markdown(summary_html, unsafe_allow_html=True)
 
     styled_html = df_result.style \
