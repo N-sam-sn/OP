3ccb6e1dd5ccf4a83105e8bc3efef31ae2c20fde
N-sam-sn <snsamsn4@gmail.com>
2026-05-08 13:43:38 +0300

    Update VP_.py

--- VP_.py
+++ VP_.py
@@ -74,7 +74,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # === ЗАГОЛОВОК ===
-st.title("Дашборд по продажам апреля 2026")
+st.title("Дашборд по продажам мая 2026")
 
 # === ЗАГРУЗКА ДАННЫХ ===
 df = load_data()
@@ -193,7 +193,7 @@ if not filtered_df.empty:
         </div>
     """
 
-    st.subheader("Результаты на 30.04.2026")
+    st.subheader("Результаты на 07.05.2026")
     st.markdown(summary_html, unsafe_allow_html=True)
 
     styled_html = df_result.style \
